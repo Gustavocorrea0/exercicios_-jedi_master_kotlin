55367f611b32b20714ce6bee5b05bd2c039faca5
Gustavo Alfredo <111892331+Gustavocorrea0@users.noreply.github.com>
2024-03-19 16:55:50 -0300

    Add files via upload

---
+++ jediMasterAtividadesKotlin/src/main/kotlin/umfg/edu/jediMaster/JoKenPo.kt
@@ -0,0 +1,16 @@
+package umfg.edu.jediMaster
+
+
+
+fun main() {
+
+    println("JoKenPô")
+
+    println(" P  - Pedra")
+    println(" A  - Papel")
+    println(" T  - Tesoura")
+    println(" >  Qual a escolha")
+    var escolhaJogador1 = readln()
+
+
+}
